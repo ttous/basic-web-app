f4c2e616ebb5b4c53c1abf6e10464c9bfa10c56d
ttoussaint <teddy.toussaint@nexworld.fr>
2019-02-26 18:52:11 +0100

    Test new Travis

--- __tests__/client/interface.test.tsx
+++ __tests__/client/interface.test.tsx
@@ -1,12 +1,26 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
-import App from "../../src/client/ts/App";
+import Home from '../../src/client/ts/pages/Home';
+import About from '../../src/client/ts/pages/About';
+import NotFound from '../../src/client/ts/pages/NotFound';
 
-describe('Default', () => {
-    test('displaying "fail"', () => {
-        const tree = renderer
-            .create(<App />)
-            .toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+test('Home', () => {
+    const tree = renderer
+        .create(<Home />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('About', () => {
+    const tree = renderer
+        .create(<About />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('NotFound', () => {
+    const tree = renderer
+        .create(<NotFound />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
 });
