84625134aece7428a4e6bcfa9f3d12b22f3bda52
ttoussaint <teddy.toussaint@nexworld.fr>
2019-02-26 01:27:17 +0100

    Removev webpack-dev-server and add Express router

--- webpack.config.js
+++ webpack.config.js
@@ -41,13 +41,10 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js", ".json"]
   },
-  devServer: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': 'http://localhost:8080'
-    }
-  },
+  // devServer: {
+  //   port: 3000,
+  //   open: true
+  // },
   plugins: [
     new CleanWebpackPlugin([outputDirectory]),
     new HtmlWebpackPlugin({
